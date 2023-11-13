ad874e0cae2059b0d28bb35b66f5bc48c473f5c3
0-isabelle <140411120+0-isabelle@users.noreply.github.com>
2023-11-13 09:18:12 -0300

    Update .script.js

--- js/.script.js
+++ js/.script.js
@@ -583,30 +583,6 @@ options: [
 
 },
 {
-numb: 5,
-question: "Quais planetas do sistema solar realizam seu movimento de
-rotação em sentido horário?",
-answer: "Vênus e Urano",
-options: [
-"Saturno e Júpiter",
-"Vênus e Urano",
-"Terra e Marte",
-"Mercúrio e Saturno"
-]
-},
-// you can uncomment the below codes and make duplicate as more as you
-want to add question
-// but remember you need to give the numb value serialize like
-1,2,3,5,6,7,8,9.....
-// {
-// numb: 6,
-// question: "Your Question is Here",
-// answer: "Correct answer of the question is here",
-// options: [
-// "Option 1",
-// "option 2",
-// "option 3",
-// "option 4"
-// ]
-// },
-];
+
+
+  
